f8da2c5a21912af879dcd920bb8dc556cf038e94
Bruno Russo <bruno@jumpstartmd.com>
2024-10-01 18:32:40 -0300

    refactor: adjust query to add correct param

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -5,6 +6,8 @@ import { Repository } from 'typeorm';
 import { TodoUser } from '../todo-user.entity';
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  private readonly logger = new Logger(JwtStrategy.name);
+
   constructor(
     @InjectRepository(TodoUser)
     private readonly userRepository: Repository<TodoUser>,
@@ -17,6 +20,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return await this.userRepository.findOne(payload.sub);
+    return await this.userRepository.findOne({
+      where: { id: payload.sub },
+    });
   }
 }
