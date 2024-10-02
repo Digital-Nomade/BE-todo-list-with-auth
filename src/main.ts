7c5a6e00ac453bf23a9fce1f6e0f99ac239ca16c
Bruno Russo <bruno@jumpstartmd.com>
2024-10-02 18:52:00 -0300

    chore: add todos reminders on main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/**
+ * TODO: Add production packages
+ * - Helmet
+ * - Swagger
+ * - Husky
+ */
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
